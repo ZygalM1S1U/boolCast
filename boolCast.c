0e90978ba845ee96c2ac82b61871e1f1119cedb6
Zygal <jnovor@comcast.net>
2018-04-27 13:44:15 -0400

    Basic template commit
    Basic templates for the main c file and an include guarded header for this commit.

---
+++ boolCast.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "boolcast.h"
+
+int main()
+{
+///placeholder
+}
---
+++ boolCast.h
@@ -0,0 +1,12 @@
+#ifndef BOOLCAST_H_   /* Include guard */
+#define BOOLCAST_H_
+
+#include <stdio.h>
+
+
+/*function prototypes*/
+///Placeholder
+int main(void);
+
+
+#endif // BOOLCAST_H_
