a4b600b74a1bad2e976ca03d01d995ac4d88e749
James Novorita <james.novorita@stryker.com>
2020-07-29 07:20:58 -0400

    Updated lib

--- boolCast.h
+++ boolCast.h
@@ -1,29 +1,44 @@
 #ifndef BOOLCAST_H_   /* Include guard */
 #define BOOLCAST_H_
 
-#include <stdio.h>
 #include <stdint.h>
 
 #define MAX_BITS    8
 #define TRUE        1
 #define FALSE       0
-//file contract
-///@note a sample of what is needed!
-//bool(x) mybool;
-//bool(x) mybool = {00110011};
-//bool(x) mybool = {8F};
-//bool(x) mybool[0] = 1;
-//bool(x) mybool = {FFTTFFTT};
+
+/**
+  @section BOOL CAST
+  @brief This is a tool to use multiple data types to represent flags as one-bit, rather than a byte to save memory.
+         It is targeted for the Nintendo 64
+  */
+
+/**
+ *  @macro BC_SET_FLAG
+ *  @brief This is a shorthand notation for setting or clearing a flag in the data.
+ *  @param FLAG - This is the entire flag data to have one bit set
+ *  @param POS -  This is the bit to set in the entire data structure
+ */
+#define BC_SET_FLAG(FLAG, POS) (FLAG |=  (1<<POS))
+
+/**
+ *  @macro BC_CLEAR_FLAG
+ *  @brief This is a shorthand notation for setting or clearing a flag in the data.
+ *  @param FLAG - This is the entire flag data to have one bit set
+ *  @param POS -  This is the bit to set in the entire data structure
+ */
+#define BC_CLEAR_FLAG(FLAG, POS) (FLAG &= ~(1<<POS))
 
 ///@brief typedef declarations
 ///@param Bool - a parameter to pass an unsigned 8-bit integer for a flag mask.
-///@note 'Bool' is used as a typical defined function here, but then replaced in memory with a bit mask as a rudimentary form of compression
-//typedef uint8_t Bool;
-typedef uint8_t bool;
-
-/*function prototypes*/
-///Placeholder
-//static void instance(void);
+///@note 'Bool' is used as a typical defined function here, as C99 standards are not yet defined
+union bool
+{
+    uint8_t  flags8_t;
+    uint16_t flags16_t;
+    uint32_t flags32_t;
+    uint64_t flags64_t;
+};
 
 
 #endif // BOOLCAST_H_
